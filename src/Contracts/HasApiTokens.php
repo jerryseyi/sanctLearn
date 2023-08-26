d7e2c67d33f50456ce1b0077a4f49b6f19db1732
Jerry Seyi <jerryseyi68@gmail.com>
2023-08-26 12:52:28 +0100

    return type morphMany to tokens

--- src/Contracts/HasApiTokens.php
+++ src/Contracts/HasApiTokens.php
@@ -11,7 +11,7 @@ interface HasApiTokens
      *
      * @return MorphMany
      */
-    public function tokens();
+    public function tokens(): MorphMany;
 
     /**
      * Determine if the current API token has a given scope.
