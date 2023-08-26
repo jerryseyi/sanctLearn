d798297b2d87bfe39ea0a3db89a9ac3fcd060c4d
Jerry Seyi <jerryseyi68@gmail.com>
2023-08-26 01:17:49 +0100

    has api tokens

---
+++ src/Contracts/HasApiTokens.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laravel\Sanctum\Contracts;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+interface HasApiTokens
+{
+    /**
+     * Get the access tokens that belongs to the model.
+     *
+     * @return MorphMany
+     */
+    public function tokens();
+
+    /**
+     * Determine if the current API token has a given scope.
+     *
+     * @param string $ability
+     * @return mixed
+     */
+    public function tokenCan(string $ability);
+}
